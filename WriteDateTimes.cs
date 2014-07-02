e21e9191344dfb252874e0a5686f139b9bceb0c6
Ivan Boykov Peev <ivan.boykov.peev@gmail.com>
2014-07-02 21:35:01 +0300

    C# WriteDateTimes.cs

---
+++ WriteDateTimes.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWorks
+{
+    class WriteDateTimes
+    {
+        //Write a program that prints on the console the current date and time.
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DateTime.Now);
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+        }
+    }
+}
