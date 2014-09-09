f8a470029bcb7feea4b499c9c967c617199041b7
Ivan Boykov Peev <ivan.boykov.peev@gmail.com>
2014-09-09 13:13:30 +0300

    Fast version

---
+++ C# HomeWorks/AllDivisibelBy5FastVersion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace AllDivisibelBy5FastVersion
+{
+    class AllDivisibelBy5FastVersion
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            //How many numbers have divisor by 5
+            int numberOfRange = (secondNumber / 5) - (firstNumber / 5);
+            if (firstNumber % 5 == 0)
+            {
+                numberOfRange++;
+            }
+            Console.WriteLine(numberOfRange);
+
+            Console.ReadKey();
+        }
+    }
+}
