2ae407706fed7128dfd65ed9ec5768869559fe7d
Ivan Boykov Peev <ivan.boykov.peev@gmail.com>
2014-09-12 20:00:23 +0300

    Program sum five numbers

---
+++ C# HomeWorks/SumFiveNums.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace SumFiveNums
+{
+    class SumFiveNums
+    {
+        static void Main(string[] args)
+        {
+            string inputLine = string.Empty;
+            int firstNum = 0;
+            bool isNumberCorect = false;
+            while (!isNumberCorect)
+            {
+                inputLine = Console.ReadLine();
+                isNumberCorect = int.TryParse(inputLine, out firstNum);
+            }
+
+            int secondNumber = 0;
+            isNumberCorect = false;
+            while (!isNumberCorect)
+            {
+                inputLine = Console.ReadLine();
+                isNumberCorect = int.TryParse(inputLine, out secondNumber);
+            }
+
+            int thirdNumber = 0;
+            isNumberCorect = false;
+            while (!isNumberCorect)
+            {
+                inputLine = Console.ReadLine();
+                isNumberCorect = int.TryParse(inputLine, out thirdNumber);
+            }
+
+            int fourtNumber = 0;
+            isNumberCorect = false;
+            while (!isNumberCorect)
+            {
+                inputLine = Console.ReadLine();
+                isNumberCorect = int.TryParse(inputLine, out fourtNumber);
+            }
+
+            int fifthNumber = 0;
+            isNumberCorect = false;
+            while (!isNumberCorect)
+            {
+                inputLine = Console.ReadLine();
+                isNumberCorect = int.TryParse(inputLine, out fifthNumber); 
+            }
+
+            long sun = (long)firstNum + secondNumber + thirdNumber +
+                fourtNumber + fifthNumber;
+            Console.WriteLine(sun);
+
+            Console.ReadKey();
+        }
+    }
+}
